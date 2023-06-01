e74e25e39331321a568a8a9bcbece3e4186f4ae1
Marc Schär <marc.schaer93@gmail.com>
2023-06-01 16:52:20 +0200

    style

--- calc/app.py
+++ calc/app.py
@@ -3,26 +3,25 @@ from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
-
-##HARDCODED for each operation Function
-
-# @app.route('/add')
-# def do_math():
-"""add a and b """
-#     a = int(request.args['a'])
-#     b = int(request.args['b'])
-#     sum  = f"{a+b}"
+##HARDCODED for each operation Function. 
+@app.route('/add')
+def add():
+    """ Add a and b and return sum as a string """
     
-#     return sum
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    sum  = f"{a+b}"
+    
+    return sum
     
 
-## BETTER
+## BETTER Version
 operators = {'add': add, "sub": sub, 'mult': mult, "div": div}
-
 
 @app.route("/math/<operation>")
 def do_math(operation):
-    """do a specific operation on a and b out of the operators dictionary """
+    """ Do a specific operation on "a" and "b" out of the operators dictionary """
+   
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     result  = operators[operation](a,b)
